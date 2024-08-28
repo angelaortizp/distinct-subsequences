8124feea3c78d75685192340d8111bd01f45e40f
angelaortizp <43590934+angelaortizp@users.noreply.github.com>
2024-08-28 15:05:52 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/com/algorithms/distinctsubsequences/DistinctsubsequencesApplication.java
@@ -0,0 +1,13 @@
+package com.algorithms.distinctsubsequences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistinctsubsequencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistinctsubsequencesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algorithms/distinctsubsequences/DistinctsubsequencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorithms.distinctsubsequences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistinctsubsequencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
