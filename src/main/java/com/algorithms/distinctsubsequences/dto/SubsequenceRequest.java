8ee245ed034e34bdad732e55565dbdd9266be4a5
angelaortizp <43590934+angelaortizp@users.noreply.github.com>
2024-08-28 17:40:54 -0500

    inicio adaptación microservicios

---
+++ src/main/java/com/algorithms/distinctsubsequences/dto/SubsequenceRequest.java
@@ -0,0 +1,24 @@
+package com.algorithms.distinctsubsequences.dto;
+
+public class SubsequenceRequest {
+
+	private String source;
+	private String target;
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getTarget() {
+		return target;
+	}
+
+	public void setTarget(String target) {
+		this.target = target;
+	}
+
+}
---
+++ src/main/java/com/algorithms/distinctsubsequences/dto/SubsequenceResponse.java
@@ -0,0 +1,20 @@
+package com.algorithms.distinctsubsequences.dto;
+
+public class SubsequenceResponse {
+
+	private int count;
+
+	public SubsequenceResponse(int count) {
+		super();
+		this.count = count;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+}
